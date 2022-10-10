ecdda9c6b13fb14774f993f62cb051189e0f971d
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-10-10 16:53:27 +0100

    using backing beans

--- src/main/java/com/nidhallourimi/hsportscatalogjsf/CatalogItemFormBean.java
+++ src/main/java/com/nidhallourimi/hsportscatalogjsf/CatalogItemFormBean.java
@@ -3,15 +3,49 @@ package com.nidhallourimi.hsportscatalogjsf;
 
 
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+//information pressist acroos http request for particular user
+@SessionScoped
 //scop this bean exists for  the request
-@RequestScoped
+//@RequestScoped
 //bean name
 @Named
-public class CatalogItemFormBean {
+public class CatalogItemFormBean implements Serializable {
 
-    private String name;
+    private CatalogItem item=new CatalogItem();
+
+    private List<CatalogItem> items=new ArrayList<>();
+    public String addItem(){
+        long itemId=  this.items.size()+1;
+        this.items.add(new CatalogItem(itemId,this.item.getName(),this.item.getManufacturer(),this.item.getDescription()
+                ,this.item.getAvailableDate()));
+        this.items.stream().forEach(item->{
+            System.out.println(item.toString());
+        });
+        return "list?faces-redirect=true";
+        //navigation preformed via a redirect
+    }
+
+    public CatalogItem getItem() {
+        return item;
+    }
+
+    public void setItem(CatalogItem item) {
+        this.item = item;
+    }
+
+    public List<CatalogItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CatalogItem> items) {
+        this.items = items;
+    }
+/*    private String name;
     private String manufacturer;
     private String sky;
 
@@ -37,5 +71,5 @@ public class CatalogItemFormBean {
 
     public void setSky(String sky) {
         this.sky = sky;
-    }
+    }*/
 }
